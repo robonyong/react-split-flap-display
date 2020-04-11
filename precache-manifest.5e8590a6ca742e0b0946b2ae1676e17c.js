8cf68ca1a7e53eb327f6d529b543f86e6ff2e1c2
Robin Yang <robinyone@gmail.com>
2020-04-11 13:17:38 -0700

    Updates

---
+++ precache-manifest.5e8590a6ca742e0b0946b2ae1676e17c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0e70115d07fa3a9e4eaf99e1292f4e",
+    "url": "/react-split-flap-display/index.html"
+  },
+  {
+    "revision": "d4da8c764a14216e8a10",
+    "url": "/react-split-flap-display/static/css/main.e1c4e39d.chunk.css"
+  },
+  {
+    "revision": "bd578efe12257f6414da",
+    "url": "/react-split-flap-display/static/js/2.337587ff.chunk.js"
+  },
+  {
+    "revision": "8f7dce1b3ddf19bbc03f099fe5916410",
+    "url": "/react-split-flap-display/static/js/2.337587ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4da8c764a14216e8a10",
+    "url": "/react-split-flap-display/static/js/main.bc8b47a3.chunk.js"
+  },
+  {
+    "revision": "936f5dd8fa9df61b87a4",
+    "url": "/react-split-flap-display/static/js/runtime-main.6428a5b6.js"
+  }
+]);
